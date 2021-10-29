cc4c34443e9e2618ca78a51066194ca80df8c51d
Kai.He <kai.he.0811@qq.com>
2021-10-29 12:50:07 +0800

    添加代码格式化工具 isort

--- butler/service/dotfile.py
+++ butler/service/dotfile.py
@@ -1,7 +1,7 @@
 import os.path
 import typing as t
 
-from butler.utils import EchoUtils, DirectoryUtils, FileUtils
+from butler.utils import DirectoryUtils, EchoUtils, FileUtils
 
 
 class DotfileService:
